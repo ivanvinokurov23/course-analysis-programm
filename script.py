1d50b9c30a38a977db011331068b93916402c9f2
ivanvinokurov23 <fizelab@yandex.ru>
2026-01-18 01:38:18 +0300

    second version

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
-N, M, Q, cw, sw, hw, tw = map(str, input().split())
-print(M)
-print(N)
-print(cw)
-print(sw)
-print(hw)
-print(tw)
-print(Q)
+N, M, Q, cw, sw, hw, tw = map(int, input().split())
+name = input()
+if M <= 0 or cw <= 0 or sw <= 0 or hw <= 0 or tw <= 0:
+    print("Во введённых данных ошибка")
+else:
+    rating = 0
+    for i in range(M):
+        a, b, c, d = map(int, input().split(','))
+        rating = rating + a * cw + b * sw + c * hw + d *g tw
+    if rating > Q:
+        print("Во введённых данных ошибка")
+    else:
+        print(f"{name} {round(rating / Q * 100)}%")
